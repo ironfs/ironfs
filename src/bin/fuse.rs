6b771cb7ebd3bf24b918f05d89a596078a56a34e
Brandon Edens <brandonedens@gmail.com>
2021-07-06 21:36:39 -0700

    Stub out much of IronFS.

---
+++ src/bin/fuse.rs
@@ -0,0 +1,320 @@
+use fuser::{
+    Filesystem, KernelConfig, MountOption, ReplyAttr, ReplyCreate, ReplyData, ReplyDirectory,
+    ReplyEmpty, ReplyEntry, ReplyOpen, ReplyStatfs, ReplyWrite, ReplyXattr, Request, TimeOrNow,
+    FUSE_ROOT_ID,
+};
+use ironfs::IronFs;
+use std::ffi::OsStr;
+use std::os::raw::c_int;
+use std::path::{Path, PathBuf};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+struct FuseIronFs(IronFs);
+
+impl Filesystem for FuseIronFs {
+    fn init(
+        &mut self,
+        _req: &Request,
+        #[allow(unused_variables)] config: &mut KernelConfig,
+    ) -> Result<(), c_int> {
+        Ok(())
+    }
+
+    fn lookup(&mut self, req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {}
+
+    fn forget(&mut self, _req: &Request, _ino: u64, _nlookup: u64) {}
+
+    fn getattr(&mut self, _req: &Request, inode: u64, reply: ReplyAttr) {}
+
+    fn setattr(
+        &mut self,
+        req: &Request,
+        inode: u64,
+        mode: Option<u32>,
+        uid: Option<u32>,
+        gid: Option<u32>,
+        size: Option<u64>,
+        atime: Option<TimeOrNow>,
+        mtime: Option<TimeOrNow>,
+        _ctime: Option<SystemTime>,
+        fh: Option<u64>,
+        _crtime: Option<SystemTime>,
+        _chgtime: Option<SystemTime>,
+        _bkuptime: Option<SystemTime>,
+        _flags: Option<u32>,
+        reply: ReplyAttr,
+    ) {
+    }
+
+    fn readlink(&mut self, _req: &Request, inode: u64, reply: ReplyData) {}
+
+    fn mknod(
+        &mut self,
+        req: &Request,
+        parent: u64,
+        name: &OsStr,
+        mut mode: u32,
+        _umask: u32,
+        _rdev: u32,
+        reply: ReplyEntry,
+    ) {
+    }
+
+    fn mkdir(
+        &mut self,
+        req: &Request,
+        parent: u64,
+        name: &OsStr,
+        mut mode: u32,
+        _umask: u32,
+        reply: ReplyEntry,
+    ) {
+        //self.mkdir(parent as u32, name);
+        // TODO attrs
+        //reply.entry(&Duration::new(0, 0),
+    }
+
+    fn unlink(&mut self, req: &Request, parent: u64, name: &OsStr, reply: ReplyEmpty) {}
+
+    fn rmdir(&mut self, req: &Request, parent: u64, name: &OsStr, reply: ReplyEmpty) {}
+
+    fn symlink(
+        &mut self,
+        req: &Request,
+        parent: u64,
+        name: &OsStr,
+        link: &Path,
+        reply: ReplyEntry,
+    ) {
+    }
+
+    fn rename(
+        &mut self,
+        req: &Request,
+        parent: u64,
+        name: &OsStr,
+        new_parent: u64,
+        new_name: &OsStr,
+        flags: u32,
+        reply: ReplyEmpty,
+    ) {
+    }
+
+    fn link(
+        &mut self,
+        req: &Request,
+        inode: u64,
+        new_parent: u64,
+        new_name: &OsStr,
+        reply: ReplyEntry,
+    ) {
+    }
+
+    fn open(&mut self, req: &Request, inode: u64, flags: i32, reply: ReplyOpen) {}
+
+    fn read(
+        &mut self,
+        _req: &Request,
+        inode: u64,
+        fh: u64,
+        offset: i64,
+        size: u32,
+        _flags: i32,
+        _lock_owner: Option<u64>,
+        reply: ReplyData,
+    ) {
+    }
+
+    fn write(
+        &mut self,
+        _req: &Request,
+        inode: u64,
+        fh: u64,
+        offset: i64,
+        data: &[u8],
+        _write_flags: u32,
+        #[allow(unused_variables)] flags: i32,
+        _lock_owner: Option<u64>,
+        reply: ReplyWrite,
+    ) {
+    }
+
+    fn release(
+        &mut self,
+        _req: &Request<'_>,
+        inode: u64,
+        _fh: u64,
+        _flags: i32,
+        _lock_owner: Option<u64>,
+        _flush: bool,
+        reply: ReplyEmpty,
+    ) {
+    }
+
+    fn opendir(&mut self, req: &Request, inode: u64, flags: i32, reply: ReplyOpen) {}
+
+    fn readdir(
+        &mut self,
+        _req: &Request,
+        inode: u64,
+        _fh: u64,
+        offset: i64,
+        mut reply: ReplyDirectory,
+    ) {
+    }
+
+    fn releasedir(
+        &mut self,
+        _req: &Request<'_>,
+        inode: u64,
+        _fh: u64,
+        _flags: i32,
+        reply: ReplyEmpty,
+    ) {
+    }
+
+    fn statfs(&mut self, _req: &Request, _ino: u64, reply: ReplyStatfs) {}
+
+    fn setxattr(
+        &mut self,
+        request: &Request<'_>,
+        inode: u64,
+        key: &OsStr,
+        value: &[u8],
+        _flags: i32,
+        _position: u32,
+        reply: ReplyEmpty,
+    ) {
+    }
+
+    fn getxattr(
+        &mut self,
+        request: &Request<'_>,
+        inode: u64,
+        key: &OsStr,
+        size: u32,
+        reply: ReplyXattr,
+    ) {
+    }
+
+    fn listxattr(&mut self, _req: &Request<'_>, inode: u64, size: u32, reply: ReplyXattr) {}
+
+    fn removexattr(&mut self, request: &Request<'_>, inode: u64, key: &OsStr, reply: ReplyEmpty) {}
+
+    fn access(&mut self, req: &Request, inode: u64, mask: i32, reply: ReplyEmpty) {}
+
+    fn create(
+        &mut self,
+        req: &Request,
+        parent: u64,
+        name: &OsStr,
+        mut mode: u32,
+        _umask: u32,
+        flags: i32,
+        reply: ReplyCreate,
+    ) {
+    }
+
+    #[cfg(target_os = "linux")]
+    fn fallocate(
+        &mut self,
+        _req: &Request<'_>,
+        inode: u64,
+        _fh: u64,
+        offset: i64,
+        length: i64,
+        mode: i32,
+        reply: ReplyEmpty,
+    ) {
+    }
+
+    fn copy_file_range(
+        &mut self,
+        _req: &Request<'_>,
+        src_inode: u64,
+        src_fh: u64,
+        src_offset: i64,
+        dest_inode: u64,
+        dest_fh: u64,
+        dest_offset: i64,
+        size: u64,
+        _flags: u32,
+        reply: ReplyWrite,
+    ) {
+    }
+}
+
+/*
+#[derive(Serialize, Deserialize)]
+struct InodeAttributes {
+    pub inode: Inode,
+    pub open_file_handles: u64, // Ref count of open file handles to this inode
+    pub size: u64,
+    pub last_accessed: (i64, u32),
+    pub last_modified: (i64, u32),
+    pub last_metadata_changed: (i64, u32),
+    pub kind: FileKind,
+    // Permissions and special mode bits
+    pub mode: u16,
+    pub hardlinks: u32,
+    pub uid: u32,
+    pub gid: u32,
+    pub xattrs: BTreeMap<Vec<u8>, Vec<u8>>,
+}
+
+impl From<InodeAttributes> for fuser::FileAttr {
+    fn from(attrs: InodeAttributes) -> Self {
+        fuser::FileAttr {
+            ino: attrs.inode,
+            size: attrs.size,
+            blocks: (attrs.size + BLOCK_SIZE - 1) / BLOCK_SIZE,
+            atime: system_time_from_time(attrs.last_accessed.0, attrs.last_accessed.1),
+            mtime: system_time_from_time(attrs.last_modified.0, attrs.last_modified.1),
+            ctime: system_time_from_time(
+                attrs.last_metadata_changed.0,
+                attrs.last_metadata_changed.1,
+            ),
+            crtime: SystemTime::UNIX_EPOCH,
+            kind: attrs.kind.into(),
+            perm: attrs.mode,
+            nlink: attrs.hardlinks,
+            uid: attrs.uid,
+            gid: attrs.gid,
+            rdev: 0,
+            blksize: BLOCK_SIZE as u32,
+            flags: 0,
+        }
+    }
+}
+*/
+
+struct RamStorage(Vec<u8>);
+
+impl RamStorage {
+    fn new(nbytes: usize) -> Self {
+        RamStorage(Vec::with_capacity(nbytes))
+    }
+}
+
+impl ironfs::Storage for RamStorage {
+    fn read(lba: u32, data: &mut [u8]) {}
+    fn write(lba: u32, data: &[u8]) {}
+    fn erase(lba: u32, num_lba: u32) {}
+}
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opt {
+    mount_point: String,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+
+    let mut options = vec![MountOption::FSName("fuser".to_string())];
+
+    let storage = RamStorage::new(33554432);
+    let ironfs = IronFs::new(storage);
+    fuser::mount2(FuseIronFs(ironfs), opt.mount_point, &options).unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,119 @@
+
+#![no_std]
+
+use zerocopy::{AsBytes, FromBytes};
+
+const DATA_BLOCK_MAGIC: BlockMagic = BlockMagic(*b"DATA");
+const INODE_BLOCK_MAGIC: BlockMagic = BlockMagic(*b"INOD");
+const EXT_INODE_BLOCK_MAGIC: BlockMagic = BlockMagic(*b"EINO");
+const SUPER_BLOCK_MAGIC: BlockMagic = BlockMagic(*b"SUPR");
+const EXT_SUPER_BLOCK_MAGIC: [u8; 12] = *b" BLK IRON FS";
+const DIR_BLOCK_MAGIC: BlockMagic = BlockMagic(*b"DIRB");
+const EXT_DIR_BLOCK_MAGIC: BlockMagic = BlockMagic(*b"EDIR");
+const FREE_BLOCK_MAGIC: BlockMagic = BlockMagic(*b"FREE");
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct BlockMagic([u8; 4]);
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct BlockId(u32);
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct Crc(u32);
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct SuperBlock {
+    magic: BlockMagic,
+    ext_magic: [u8; 12],
+    version: u32,
+    root_dir_block: BlockId,
+    num_blocks: u32,
+    block_size: u32,
+    created_on: u32,
+    crc: Crc,
+}
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct DirBlock {
+    magic: BlockMagic,
+    next_dir_block: BlockId,
+    name: [u8; 256],
+    owner: u16,
+    group: u16,
+    perms: u16,
+    reserved: u16,
+    content_blocks: [BlockId; 955],
+    crc: Crc,
+}
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct ExtDirBlock {
+    magic: BlockMagic,
+    next_dir_block: BlockId,
+    data: [BlockId; 1021],
+    crc: Crc,
+}
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct DataBlock {
+    magic: BlockMagic,
+    data: [u8; 4088],
+    crc: Crc,
+}
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct InodeBlock {
+    magic: BlockMagic,
+    next_inode: BlockId,
+    name: [u8; 256],
+    owner: u16,
+    group: u16,
+    perms: u16,
+    reserved: u16,
+    data: [u8; 1024],
+    blocks: [BlockId; 699],
+    crc: Crc,
+}
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct ExtInodeBlock {
+    magic: BlockMagic,
+    next_inode: BlockId,
+    blocks: [BlockId; 1021],
+    crc: Crc,
+}
+
+#[derive(AsBytes, FromBytes)]
+#[repr(C)]
+struct FreeBlock {
+    magic: BlockMagic,
+    next_free: BlockId,
+    crc: Crc,
+}
+
+pub trait Storage {
+    fn read(lba: u32, data: &mut [u8]);
+    fn write(lba: u32, data: &[u8]);
+    fn erase(lba: u32, num_lba: u32);
+}
+
+pub struct IronFs;
+
+impl IronFs {
+    pub fn new(storage: impl Storage) -> Self {
+        IronFs
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
